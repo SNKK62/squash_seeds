c76fd335f3e5adb3c4c885677ea69944d985e69e
SNKK62 <koki.seno62@gmail.com>
2024-04-03 23:07:44 +0900

    add player seeds

---
+++ prisma/players.seed.ts
@@ -0,0 +1,21 @@
+import { PrismaClient } from "@prisma/client";
+import data from "./data/players.json";
+
+export const setPlayerData = async (prisma: PrismaClient) => {
+  try {
+    data.forEach(async (player) => {
+      await prisma.player.create({
+        data: {
+          firstName: player.firstName,
+          lastName: player.lastName,
+          universityId: player.universityId,
+          grade: player.grade,
+          sex: player.sex,
+        },
+      });
+    });
+  } catch (e) {
+    console.error(e);
+    throw e;
+  }
+};
--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,8 +1,13 @@
 import { PrismaClient } from "@prisma/client";
+import { setUniversityData } from "./university.seed";
+import { setPlayerData } from "./players.seed";
 
 const prisma = new PrismaClient();
 
-async function main() {}
+async function main() {
+  await setUniversityData(prisma);
+  await setPlayerData(prisma);
+}
 
 main()
   .catch((e) => {
