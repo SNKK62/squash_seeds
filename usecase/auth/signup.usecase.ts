5503308904fde7fa0ec0b4d29112f7b462e07b6b
SNKK62 <koki.seno62@gmail.com>
2024-04-03 23:17:37 +0900

    update usecase input types

--- usecase/auth/signup.usecase.ts
+++ usecase/auth/signup.usecase.ts
@@ -3,13 +3,15 @@ import { CreateGakurenInput } from "@repository/gakuren.repo";
 import { Repo } from "@repository/repository";
 import { AuthService } from "@service/auth.service";
 
+type SignupUsecaseInput = CreateGakurenInput;
+
 export class SignupUsecase {
   private readonly authService: AuthService;
   constructor(repo: Repo) {
     this.authService = new AuthService(repo);
   }
 
-  public async execute(input: CreateGakurenInput): Promise<Gakuren> {
+  public async execute(input: SignupUsecaseInput): Promise<Gakuren> {
     try {
       return await this.authService.signup(input);
     } catch (e) {
--- usecase/tournament/matches/postMatches.usecase.ts
+++ usecase/tournament/matches/postMatches.usecase.ts
@@ -2,9 +2,11 @@ import { Match } from "@model/match.model";
 import { CreateMatchInput } from "@repository/match.repo";
 import { Repo } from "@repository/repository";
 
+type PostMatchesUsecaseInput = CreateMatchInput;
+
 export class PostMatchesUsecase {
   constructor(private readonly repo: Repo) {}
-  public async execute(input: CreateMatchInput): Promise<Match> {
+  public async execute(input: PostMatchesUsecaseInput): Promise<Match> {
     try {
       return await this.repo.match.createMatch(input);
     } catch (e) {
