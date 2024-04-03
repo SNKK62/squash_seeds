57a37fa73c05b85c19c9c073318b9400b3000e6e
SNKK62 <koki.seno62@gmail.com>
2024-04-03 23:07:34 +0900

    add university seed

---
+++ prisma/university.seed.ts
@@ -0,0 +1,20 @@
+import { PrismaClient } from "@prisma/client";
+import data from "./data/universities.json";
+
+export const setUniversityData = async (prisma: PrismaClient) => {
+  try {
+    data.forEach(async (university) => {
+      await prisma.university.create({
+        data: {
+          id: university.id,
+          name: university.name,
+          shortName: university.shortName,
+          region: university.region,
+        },
+      });
+    });
+  } catch (e) {
+    console.error(e);
+    throw e;
+  }
+};
