c327b4a06796e364e57d14c0cb9b2df511490e91
SNKK62 <koki.seno62@gmail.com>
2024-04-03 17:31:38 +0900

    add matchMeta usecase

--- infrastructure/db/repository/matchMeta/index.ts
+++ infrastructure/db/repository/matchMeta/index.ts
@@ -1,19 +1,46 @@
 import { prisma } from "@infrastructure/db/client";
+import { convertToMatchMeta } from "@infrastructure/db/converters/matchMeta";
+import { MatchMeta } from "@model/matchMeta.model";
 import {
   CreateMatchMetaInput,
   IMatchMetaRepo,
 } from "@repository/matchMeta.repo";
 
-const createMatchMeta = async (input: CreateMatchMetaInput): Promise<void> => {
+const getMatchMetasByTournamentId = async (
+  tournamentId: string
+): Promise<MatchMeta[]> => {
   try {
-    await prisma.matchMeta.create({
+    const matchMetas = await prisma.matchMeta.findMany({
+      where: {
+        tournamentId,
+      },
+      include: {
+        tournament: true,
+      },
+    });
+    return matchMetas.map((matchMeta) => convertToMatchMeta(matchMeta));
+  } catch (e) {
+    console.error(e);
+    throw e;
+  }
+};
+
+const createMatchMeta = async (
+  input: CreateMatchMetaInput
+): Promise<MatchMeta> => {
+  try {
+    const matchMeta = await prisma.matchMeta.create({
       data: {
         type: input.type,
         isRated: input.isRated,
         sex: input.sex,
         tournamentId: input.tournamentId,
       },
+      include: {
+        tournament: true,
+      },
     });
+    return convertToMatchMeta(matchMeta);
   } catch (e) {
     console.error(e);
     throw e;
@@ -21,5 +48,6 @@ const createMatchMeta = async (input: CreateMatchMetaInput): Promise<void> => {
 };
 
 export const MatchMetaRepo: IMatchMetaRepo = {
+  getMatchMetasByTournamentId,
   createMatchMeta,
 };
---
+++ usecase/tournament/matchMetas/getOpenMatchMetas.usecase.ts
@@ -0,0 +1,17 @@
+import { MatchMeta } from "@model/matchMeta.model";
+import { Repo } from "@repository/repository";
+
+export class GetOpenMatchMetasUsecase {
+  constructor(private readonly repo: Repo) {}
+
+  async execute(tournamentId: string): Promise<MatchMeta[]> {
+    try {
+      return await this.repo.matchMeta.getMatchMetasByTournamentId(
+        tournamentId
+      );
+    } catch (e) {
+      console.error(e);
+      throw e;
+    }
+  }
+}
