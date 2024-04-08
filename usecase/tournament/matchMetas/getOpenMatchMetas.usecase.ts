440f838aa6ff8362125ab6d2e2f4ff3ff50c9565
SNKK62 <koki.seno62@gmail.com>
2024-04-08 23:37:35 +0900

    fix match and matchMeta usecase

--- usecase/tournament/matchMetas/getOpenMatchMetas.usecase.ts
+++ usecase/tournament/matchMetas/getOpenMatchMetas.usecase.ts
@@ -1,13 +1,21 @@
 import { MatchMeta } from "@model/matchMeta.model";
 import { Repo } from "@repository/repository";
+import { AuthService } from "@service/auth.service";
 
 export class GetOpenMatchMetasUsecase {
-  constructor(private readonly repo: Repo) {}
+  private readonly service: AuthService;
+  constructor(private readonly repo: Repo) {
+    this.service = new AuthService(repo);
+  }
 
-  async execute(tournamentId: string): Promise<MatchMeta[]> {
+  async execute(email: string, password: string): Promise<MatchMeta[]> {
     try {
+      const self = await this.service.login(email, password);
+      const tournament = await this.repo.tournament.getOpenTournamentByRegion(
+        self.gakuren.region
+      );
       return await this.repo.matchMeta.getMatchMetasByTournamentId(
-        tournamentId
+        tournament.id
       );
     } catch (e) {
       throw e;
--- usecase/tournament/matches/getAllMatchesNotAnnounced.usecase.ts
+++ usecase/tournament/matches/getAllMatchesNotAnnounced.usecase.ts
@@ -1,12 +1,20 @@
 import { Match } from "@model/match.model";
 import { Repo } from "@repository/repository";
+import { AuthService } from "@service/auth.service";
 
 export class GetAllMatchesNotAnnouncedUsecase {
-  constructor(private readonly repo: Repo) {}
-  public async execute(tournamentId: string): Promise<Match[]> {
+  private readonly service: AuthService;
+  constructor(private readonly repo: Repo) {
+    this.service = new AuthService(repo);
+  }
+  public async execute(email: string, password: string): Promise<Match[]> {
     try {
+      const self = await this.service.login(email, password);
+      const tournament = await this.repo.tournament.getOpenTournamentByRegion(
+        self.gakuren.region
+      );
       return await this.repo.match.getMatchesNotAnnouncedByTournamentId(
-        tournamentId
+        tournament.id
       );
     } catch (e) {
       throw e;
