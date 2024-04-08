0a605ae0d58110fed3ff7fbb411cef07de94f818
SNKK62 <koki.seno62@gmail.com>
2024-04-08 23:52:58 +0900

    implement getAllMatchesNotAnnounced handler

---
+++ app/api/tournament/matches/route.ts
@@ -0,0 +1,28 @@
+import { repository } from "@registry/repository";
+import { GetAllMatchesNotAnnouncedUsecase } from "@usecase/tournament/matches/getAllMatchesNotAnnounced.usecase";
+import { cookies } from "next/headers";
+
+const getAllMatchesNotAnnouncedUsecase = new GetAllMatchesNotAnnouncedUsecase(
+  repository
+);
+
+export async function GET() {
+  try {
+    const cookieStore = cookies();
+    const email = cookieStore.get("email");
+    const password = cookieStore.get("password");
+    if (!email || !password) {
+      return Response.json({ error: "Unauthorized" }, { status: 401 });
+    }
+    const matches = await getAllMatchesNotAnnouncedUsecase.execute(
+      email.value,
+      password.value
+    );
+    return Response.json({
+      data: matches,
+    });
+  } catch (e) {
+    console.error(e);
+    return Response.json({ error: String(e) }, { status: 500 });
+  }
+}
