df7c2aedddb9254131a5e00f164c4757caa895c5
SNKK62 <koki.seno62@gmail.com>
2024-04-08 22:01:48 +0900

    add password validation in getGakurenSelf

--- app/api/gakuren/self/route.ts
+++ app/api/gakuren/self/route.ts
@@ -1,5 +1,3 @@
-import { Gakuren } from "@model/gakuren.model";
-import { University } from "@model/university.model";
 import { repository } from "@registry/repository";
 import { GetGakurenSelfUsecase } from "@usecase/gakuren/getGakurenSelf.usecase";
 import { cookies } from "next/headers";
@@ -9,24 +7,17 @@ const getGakurenSelfUsecase = new GetGakurenSelfUsecase(repository);
 export async function GET() {
   const cookieStore = cookies();
   const email = cookieStore.get("email");
-  console.log(email);
+  const password = cookieStore.get("password");
   try {
-    if (!email) {
+    if (!email || !password) {
       throw new Error("Unauthorized");
     }
-    console.log(email);
-    // const data = await getGakurenSelfUsecase.execute(email.value);
-    const university = new University(1, "東京大学", "東大", "関東");
-    const data = new Gakuren(
-      "aa",
-      "田中",
-      "太郎",
-      2,
-      university,
-      "普通",
-      "関東"
+    const gakurenWithAuthData = await getGakurenSelfUsecase.execute(
+      email.value,
+      password.value
     );
-    return Response.json({ data });
+    console.log(gakurenWithAuthData);
+    return Response.json({ data: gakurenWithAuthData.gakuren });
   } catch (e) {
     console.error(e);
     return Response.json({ error: String(e) }, { status: 500 });
--- usecase/gakuren/getGakurenSelf.usecase.ts
+++ usecase/gakuren/getGakurenSelf.usecase.ts
@@ -1,12 +1,16 @@
 import { GakurenWithAuthData } from "@repository/gakuren.repo";
 import { Repo } from "@repository/repository";
+import { AuthService } from "@service/auth.service";
 
 export class GetGakurenSelfUsecase {
-  constructor(private repo: Repo) {}
+  private readonly service: AuthService;
+  constructor(repo: Repo) {
+    this.service = new AuthService(repo);
+  }
 
-  async execute(email: string): Promise<GakurenWithAuthData> {
+  async execute(email: string, password: string): Promise<GakurenWithAuthData> {
     try {
-      return await this.repo.gakuren.getGakurenWithAuthDataByEmail(email);
+      return await this.service.login(email, password);
     } catch (e) {
       throw e;
     }
