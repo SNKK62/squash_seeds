4ea99bb62bc09a53cab549db697db072ebc934c1
SNKK62 <koki.seno62@gmail.com>
2024-04-15 00:28:19 +0900

    implement createTournament schema

---
+++ app/actions/schema/createTournament.schema.ts
@@ -0,0 +1,17 @@
+import { isSex } from "@model/sex";
+import { z } from "zod";
+
+export const createMatchMetaSchema = z.object({
+  type: z.string(),
+  isRated: z.preprocess((input) => JSON.parse(`${input}`), z.boolean()),
+  sex: z.string().refine(isSex, { message: "性別を入力してください" }),
+});
+
+export const createTournamentSchema = z.object({
+  name: z.string(),
+  region: z.string(),
+  beginAt: z.string(),
+  endAt: z.string(),
+  isTeam: z.preprocess((input) => JSON.parse(`${input}`), z.boolean()),
+  matchMetas: z.array(createMatchMetaSchema),
+});
