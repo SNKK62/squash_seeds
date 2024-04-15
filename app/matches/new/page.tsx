74554d6809e3dd672966f556940f77d7c292620d
SNKK62 <koki.seno62@gmail.com>
2024-04-16 01:15:22 +0900

    refine /matches/new

--- app/matches/new/page.tsx
+++ app/matches/new/page.tsx
@@ -2,22 +2,17 @@ import { CreateMatchForm } from "@/components/form/createMatchForm";
 import { cookies } from "next/headers";
 
 async function CreateMatchPage() {
-  try {
-    const res = await fetch(`${process.env["ORIGIN"]}/api/tournament/open`, {
-      cache: "no-store",
-      headers: {
-        Cookie: cookies().toString(),
-      },
-    });
-    const json = await res.json();
-    if (res.status !== 200) {
-      throw new Error(json.error);
-    }
-
-    return <CreateMatchForm />;
-  } catch (e) {
+  const res = await fetch(`${process.env["ORIGIN"]}/api/tournament/open`, {
+    cache: "no-store",
+    headers: {
+      Cookie: cookies().toString(),
+    },
+  });
+  if (res.status !== 200) {
     return <div>開催されている大会はありません</div>;
   }
+
+  return <CreateMatchForm />;
 }
 
 export default CreateMatchPage;
