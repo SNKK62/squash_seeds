a28f828933801acacab0cad4616f494476d1ac2b
SNKK62 <koki.seno62@gmail.com>
2024-04-15 14:54:33 +0900

    fix postMatches usecase

--- usecase/matches/postMatches.usecase.ts
+++ usecase/matches/postMatches.usecase.ts
@@ -1,14 +1,28 @@
 import { Match } from "@model/match.model";
 import { CreateMatchInput } from "@repository/match.repo";
 import { Repo } from "@repository/repository";
+import { AuthService } from "@service/auth.service";
 
-type PostMatchesUsecaseInput = CreateMatchInput;
+type PostMatchesUsecaseInput = Omit<CreateMatchInput, "tournamentId"> & {
+  email: string;
+  password: string;
+};
 
 export class PostMatchesUsecase {
-  constructor(private readonly repo: Repo) {}
+  private readonly service: AuthService;
+  constructor(private readonly repo: Repo) {
+    this.service = new AuthService(repo);
+  }
   public async execute(input: PostMatchesUsecaseInput): Promise<Match> {
     try {
-      return await this.repo.match.createMatch(input);
+      const self = await this.service.login(input.email, input.password);
+      const tournament = await this.repo.tournament.getOpenTournamentByRegion(
+        self.gakuren.region
+      );
+      return await this.repo.match.createMatch({
+        ...input,
+        tournamentId: tournament.id,
+      });
     } catch (e) {
       throw e;
     }
