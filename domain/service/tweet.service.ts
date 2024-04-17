36ffa74b242033f6be641184dbb19e800453be28
SNKK62 <koki.seno62@gmail.com>
2024-04-18 00:04:08 +0900

    add twitter-api-v2 package

--- domain/service/tweet.service.ts
+++ domain/service/tweet.service.ts
@@ -1,5 +1,15 @@
 import { IMatchRepo } from "@repository/match.repo";
 import { Repo } from "@repository/repository";
+import { TwitterApi } from "twitter-api-v2";
+
+const client = new TwitterApi({
+  // these two values come from your app's API keys
+  appKey: process.env["X_API_KEY"] as string,
+  appSecret: process.env["X_API_KEY_SECRET"] as string,
+  // these two values come from the user's access tokens
+  accessToken: process.env["X_ACCESS_TOKEN"] as string,
+  accessSecret: process.env["X_ACCESS_TOKEN_SECRET"] as string,
+});
 
 export class TweetService {
   private readonly matchRepo: IMatchRepo;
@@ -10,11 +20,8 @@ export class TweetService {
 
   public async tweet(ids: string[]): Promise<void> {
     try {
-      const promises: Promise<void>[] = [];
-      ids.map(async (id) => {
-        promises.push(this.matchRepo.announceMatch(id));
-      });
-      await Promise.all(promises);
+      const res = await client.v2.tweet("Hello world from the v2 Twitter API!");
+      console.log(res);
     } catch (e) {
       throw e;
     }
