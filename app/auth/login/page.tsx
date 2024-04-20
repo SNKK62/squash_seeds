94ac8ef5993612cff3b2fccb663e9839f7f78bb9
SNKK62 <koki.seno62@gmail.com>
2024-04-20 22:41:48 +0900

    complete login form

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -1,19 +1,7 @@
-import Link from "next/link";
-import React from "react";
-
 import { LoginForm } from "@/components/form/loginForm";
 
 function LoginPage() {
-  return (
-    <>
-      <LoginForm />
-      <div className="flex justify-center">
-        <Link href="/auth/signup" className="text-blue-500 underline">
-          初めての方はこちら
-        </Link>
-      </div>
-    </>
-  );
+  return <LoginForm />;
 }
 
 export default LoginPage;
--- components/form/loginForm.tsx
+++ components/form/loginForm.tsx
@@ -2,8 +2,10 @@
 
 import { useForm } from "@conform-to/react";
 import { parseWithZod } from "@conform-to/zod";
+import Link from "next/link";
 import { useFormState } from "react-dom";
 
+import { Warn } from "@/components/form/warn";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
@@ -22,30 +24,47 @@ export function LoginForm() {
   });
 
   return (
-    <div className="min-w-svw mt-4">
-      <div className="p-4 text-center text-3xl font-bold">ログイン</div>
-      <form
-        className="m-auto min-w-fit max-w-md space-y-2 p-4"
-        id={form.id}
-        onSubmit={form.onSubmit}
-        action={action}
-        noValidate
-      >
-        <ul>{form.errors?.map((error) => <li key={error}>{error}</li>)}</ul>
-        <div>
-          <Label htmlFor={fields.email.id}>Email</Label>
-          <Input type="email" name={fields.email.name} />
-          <div>{fields.email.errors}</div>
-        </div>
-        <div>
-          <Label htmlFor={fields.password.id}>Password</Label>
-          <Input type="password" name={fields.password.name} />
-          <div>{fields.password.errors}</div>
-        </div>
-        <div className="flex justify-center">
-          <Button variant="default">Login</Button>
-        </div>
-      </form>
-    </div>
+    <>
+      <div className="mt-4">
+        <div className="p-4 text-center text-3xl font-bold">ログイン</div>
+        <form
+          className="m-auto min-w-fit max-w-md space-y-2 p-4"
+          id={form.id}
+          onSubmit={form.onSubmit}
+          action={action}
+          noValidate
+        >
+          <ul>
+            {form.errors?.map((error) => (
+              <li key={error}>
+                <Warn>{error}</Warn>
+              </li>
+            ))}
+          </ul>
+          <div>
+            <Label htmlFor={fields.email.id}>Email</Label>
+            <Input type="email" id={fields.email.id} name={fields.email.name} />
+            <Warn>{fields.email.errors}</Warn>
+          </div>
+          <div>
+            <Label htmlFor={fields.password.id}>Password</Label>
+            <Input
+              id={fields.password.id}
+              type="password"
+              name={fields.password.name}
+            />
+            <Warn>{fields.password.errors}</Warn>
+          </div>
+          <div className="flex justify-center">
+            <Button variant="default">Login</Button>
+          </div>
+        </form>
+      </div>
+      <div className="flex justify-center">
+        <Link href="/auth/signup" className="text-blue-500 underline">
+          初めての方はこちら
+        </Link>
+      </div>
+    </>
   );
 }
