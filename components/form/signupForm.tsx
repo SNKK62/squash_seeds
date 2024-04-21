1b990b280b56bd8ae140692679432e1c61388413
SNKK62 <koki.seno62@gmail.com>
2024-04-22 00:13:36 +0900

    refine layout of signup form component

--- components/form/signupForm.tsx
+++ components/form/signupForm.tsx
@@ -52,88 +52,94 @@ export function SignupForm({ universitiesJson }: SignupFormProps) {
   return (
     <>
       <div className="mt-4">
-        <div className="p-4 text-center text-3xl font-bold">学連員登録</div>
+        <div className="pt-4 text-center text-3xl font-bold">学連員登録</div>
         <form
-          className="m-auto min-w-fit max-w-md space-y-2 p-4"
+          className="m-auto p-4"
           id={form.id}
           onSubmit={form.onSubmit}
           action={action}
           noValidate
         >
-          <ul>
+          <ul className="pb-4 text-center">
             {form.errors?.map((error) => (
               <li key={error}>
                 <Warn>{error}</Warn>
               </li>
             ))}
           </ul>
-          <div>
-            <Label htmlFor={fields.lastName.id}>苗字</Label>
-            <Input
-              type="text"
-              id={fields.lastName.id}
-              name={fields.lastName.name}
-              className="block"
-            />
-            <Warn>{fields.lastName.errors}</Warn>
-          </div>
-          <div>
-            <Label htmlFor={fields.firstName.id}>名前</Label>
-            <Input
-              type="text"
-              id={fields.firstName.id}
-              name={fields.firstName.name}
-            />
-            <Warn>{fields.firstName.errors}</Warn>
-          </div>
-          <div>
-            <div className="flex gap-4">
-              <Label className="flex items-center" htmlFor={fields.grade.id}>
-                学年
-              </Label>
+          <div className="m-auto max-w-lg px-4">
+            <div>
+              <Label htmlFor={fields.lastName.id}>苗字</Label>
               <Input
-                className="inline w-16"
-                type="number"
-                id={fields.grade.id}
-                name={fields.grade.name}
+                type="text"
+                id={fields.lastName.id}
+                name={fields.lastName.name}
+                className="block"
               />
+              <Warn>{fields.lastName.errors}</Warn>
             </div>
-            <Warn>{fields.grade.errors}</Warn>
-          </div>
-          <div>
-            <Label className="mr-4" htmlFor={fields.universityId.id}>
-              大学
-            </Label>
-            <Combobox
-              id={fields.universityId.id}
-              dataList={universityLabels}
-              control={universityControl}
-              label="University"
-            />
-            <Warn>{fields.universityId.errors}</Warn>
-          </div>
-          <div>
-            <Input type="hidden" value="普通" name={fields.role.name} />
-          </div>
-          <div>
-            <Input type="hidden" value="関東" name={fields.region.name} />
-          </div>
-          <div>
-            <Label htmlFor={fields.email.id}>Email</Label>
-            <Input id={fields.email.id} type="email" name={fields.email.name} />
-            <Warn>{fields.email.errors}</Warn>
-          </div>
-          <div>
-            <Label htmlFor={fields.password.id}>Password</Label>
-            <Input
-              type="password"
-              id={fields.password.id}
-              name={fields.password.name}
-            />
-            <Warn>{fields.password.errors}</Warn>
-          </div>
-          <div className="flex justify-center">
-            <Button variant="default">Signup</Button>
+            <div>
+              <Label htmlFor={fields.firstName.id}>名前</Label>
+              <Input
+                type="text"
+                id={fields.firstName.id}
+                name={fields.firstName.name}
+              />
+              <Warn>{fields.firstName.errors}</Warn>
+            </div>
+            <div>
+              <div className="flex gap-4">
+                <Label className="flex items-center" htmlFor={fields.grade.id}>
+                  学年
+                </Label>
+                <Input
+                  className="inline w-16"
+                  type="number"
+                  id={fields.grade.id}
+                  name={fields.grade.name}
+                />
+              </div>
+              <Warn>{fields.grade.errors}</Warn>
+            </div>
+            <div>
+              <Label className="mr-4" htmlFor={fields.universityId.id}>
+                大学
+              </Label>
+              <Combobox
+                id={fields.universityId.id}
+                dataList={universityLabels}
+                control={universityControl}
+                label="大学"
+              />
+              <Warn>{fields.universityId.errors}</Warn>
+            </div>
+            <div>
+              <Input type="hidden" value="普通" name={fields.role.name} />
+            </div>
+            <div>
+              <Input type="hidden" value="関東" name={fields.region.name} />
+            </div>
+            <div>
+              <Label htmlFor={fields.email.id}>Email</Label>
+              <Input
+                id={fields.email.id}
+                type="email"
+                name={fields.email.name}
+              />
+              <Warn>{fields.email.errors}</Warn>
+            </div>
+            <div>
+              <Label htmlFor={fields.password.id}>Password</Label>
+              <Input
+                type="password"
+                id={fields.password.id}
+                name={fields.password.name}
+              />
+              <Warn>{fields.password.errors}</Warn>
+            </div>
+            <div className="flex justify-center">
+              <Button variant="default">サインアップ</Button>
+            </div>
           </div>
         </form>
       </div>
