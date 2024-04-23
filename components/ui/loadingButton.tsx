d881c6e854988ec9cb7bb5d8bd140021dca838a2
SNKK62 <koki.seno62@gmail.com>
2024-04-23 14:01:54 +0900

    update loading icon

--- components/ui/loadingButton.tsx
+++ components/ui/loadingButton.tsx
@@ -1,4 +1,4 @@
-import { ReloadIcon } from "@radix-ui/react-icons";
+import { UpdateIcon } from "@radix-ui/react-icons";
 import React from "react";
 
 import { Button, ButtonProps } from "@/components/ui/button";
@@ -13,7 +13,7 @@ export const LoadingButton = React.forwardRef<
 >(({ loading, children, ...props }, ref) => {
   return (
     <Button {...props} disabled={loading} ref={ref}>
-      {loading && <ReloadIcon className="mr-2 size-4 animate-spin" />}
+      {loading && <UpdateIcon className="mr-2 size-4 animate-spin" />}
       {children}
     </Button>
   );
