349e6b048253a8d3c898380d60bde56591d275d9
SNKK62 <koki.seno62@gmail.com>
2024-07-30 14:14:04 +0900

    add announceMatches in tweet usecase

--- usecase/matches/tweetMatches.usecase.ts
+++ usecase/matches/tweetMatches.usecase.ts
@@ -1,14 +1,21 @@
+import { IMatchRepo } from "@repository/match.repo";
 import { Repo } from "@repository/repository";
 import { TweetService } from "@service/tweet.service";
 
 export class TweetMatchesUsecase {
   private readonly service: TweetService;
+  private readonly matchRepo: IMatchRepo;
   constructor(repo: Repo) {
     this.service = new TweetService(repo);
+    this.matchRepo = repo.match;
   }
   public async execute(ids: string[]): Promise<{ message: string }> {
     try {
       await this.service.tweet(ids);
+      const announceResults = ids.map((id) => {
+        return this.matchRepo.announceMatch(id);
+      });
+      await Promise.all(announceResults);
       return {
         message: "Tweeted successfully",
       };
