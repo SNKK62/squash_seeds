f81b1904eac074d751c005b46be6a52c23b9b50d
SNKK62 <koki.seno62@gmail.com>
2024-07-31 12:43:22 +0900

    update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
-export default function Home() {
+export default async function Home() {
   return (
-    <main className='flex min-h-screen flex-col items-center justify-between p-24'>
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
       main
     </main>
   );
