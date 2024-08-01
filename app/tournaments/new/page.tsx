40da03a16dd827827fdc3be25459333e1e7d482b
SNKK62 <koki.seno62@gmail.com>
2024-08-02 00:37:39 +0900

    implement auth for tournament creation

--- app/tournaments/new/page.tsx
+++ app/tournaments/new/page.tsx
@@ -1,11 +1,33 @@
+import { cookies } from "next/headers";
+
 import { CreateTournamentForm } from "@/components/form/createTournamentForm";
 
-function CreateTournamentPage() {
-  return (
-    <>
-      <CreateTournamentForm />
-    </>
-  );
+async function CreateTournamentPage() {
+  let isAuth = false;
+  try {
+    const res = await fetch(
+      `${process.env["NEXT_PUBLIC_ORIGIN"]}/api/auth/login`,
+      {
+        cache: "no-store",
+        headers: {
+          Cookie: cookies().toString(),
+        },
+      }
+    );
+    if (!res.ok) {
+      throw new Error("Not authorized");
+    }
+
+    const data = (await res.json()).data;
+
+    if (data.role !== "幹部") {
+      throw new Error("Not authorized");
+    }
+    isAuth = true;
+  } catch {
+    isAuth = false;
+  }
+  return <>{isAuth ? <div>権限がありません</div> : <CreateTournamentForm />}</>;
 }
 
 export default CreateTournamentPage;
