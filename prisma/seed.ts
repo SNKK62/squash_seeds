121ceb4cc8fd76165335065d8daac8e2c4d70c53
SNKK62 <koki.seno62@gmail.com>
2024-11-27 00:02:09 +0900

    fix error in deployment

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,6 +1,6 @@
 import { PrismaClient } from "@prisma/client";
 
-// import { setPlayerData } from "./seeds/players.seed";
+import { setPlayerData } from "./seeds/players.seed";
 import { setUniversityData } from "./seeds/university.seed";
 
 const prisma = new PrismaClient();
@@ -8,7 +8,7 @@ const prisma = new PrismaClient();
 async function main() {
   await setUniversityData(prisma);
   setTimeout(async () => {
-    // await setPlayerData(prisma);
+    await setPlayerData(prisma);
   }, 2000);
 }
 
