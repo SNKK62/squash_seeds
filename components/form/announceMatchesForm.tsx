5ff6924dba5a3cc668a5dc7659617c2ccc5eedfa
SNKK62 <koki.seno62@gmail.com>
2024-11-28 02:22:30 +0900

    enable to copy tweet text

--- components/form/announceMatchesForm.tsx
+++ components/form/announceMatchesForm.tsx
@@ -1,14 +1,32 @@
 "use client";
-import { useState } from "react";
+import { useCallback, useMemo, useState } from "react";
 
 import { Warn } from "@/components/form/warn";
 import { MatchesTable } from "@/components/table/matchesTable";
+import { Button } from "@/components/ui/button";
 import { LoadingButton } from "@/components/ui/loadingButton";
 
 import { Match, MatchJSON } from "@model/match.model";
 
 interface AnnounceMatchesFormProps {
   matchesJson: MatchJSON[];
+}
+
+function calculateTweetLength(text: string): number {
+  let length = 0;
+  for (const char of text) {
+    if (char === "\n") {
+      // 改行文字
+      length += 2;
+    } else if (char.match(/[ -~]/)) {
+      // 半角文字
+      length += 1;
+    } else {
+      // 全角文字
+      length += 2;
+    }
+  }
+  return length;
 }
 
 function AnnounceMatchesForm({ matchesJson }: AnnounceMatchesFormProps) {
@@ -18,6 +36,51 @@ function AnnounceMatchesForm({ matchesJson }: AnnounceMatchesFormProps) {
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState("");
   const [selectedIds, setSelectedIds] = useState<string[]>([]);
+  const [showResult, setShowResult] = useState(false);
+  const tweetText = useMemo(() => {
+    const selectedMatches = selectedIds.map((id) => {
+      return matches.find((match) => match.id === id)!;
+    });
+    const matchesNotAnnounced: Match[] = selectedMatches.filter((match) => {
+      return !match.isAnnounced;
+    });
+    // const tournamentName = matchesNotAnnounced[0]?.tournament.name;
+    const resultDividedByMatchMeta: Record<string, string[]> = {};
+    matchesNotAnnounced.forEach((match) => {
+      const matchMetaType = match.matchMeta.type;
+      if (!resultDividedByMatchMeta[matchMetaType]) {
+        resultDividedByMatchMeta[matchMetaType] = [];
+      }
+      resultDividedByMatchMeta[matchMetaType].push(match.formattedScore);
+    });
+    // save the tweet text length
+    // let tweetText: string = tournamentName ? `【${tournamentName}】\n` : "";
+    let tweetText: string = "";
+    tweetText += Object.entries(resultDividedByMatchMeta)
+      .map(([key, value]) => {
+        let text = `${key}`;
+        value.map((el) => {
+          text += `\n${el}`;
+        });
+        text += `\n`;
+        return text;
+      })
+      .join("\n");
+    return tweetText;
+  }, [selectedIds, matches]);
+
+  const textLength = useMemo(
+    () => calculateTweetLength(tweetText),
+    [tweetText]
+  );
+
+  const handleClickShowResult = useCallback(() => {
+    setShowResult((prev) => !prev);
+  }, []);
+
+  const handleClickCopy = useCallback(() => {
+    navigator.clipboard.writeText(tweetText);
+  }, [tweetText]);
 
   const handleClick = () => {
     setLoading(true);
@@ -57,7 +120,7 @@ function AnnounceMatchesForm({ matchesJson }: AnnounceMatchesFormProps) {
   };
 
   return (
-    <div className="w-dvw">
+    <div className="box-border w-full">
       <div className="mx-2">
         <MatchesTable
           matches={matches}
@@ -85,6 +148,27 @@ function AnnounceMatchesForm({ matchesJson }: AnnounceMatchesFormProps) {
       <div className="text-center">
         <Warn>{error}</Warn>
       </div>
+      <div className="flex justify-center">
+        <Button onClick={handleClickShowResult} className="my-4">
+          {showResult ? "Hide" : "Show"} Result
+        </Button>
+      </div>
+      {showResult && (
+        <div className="m-8 mt-0">
+          <div className="flex justify-center">
+            <Button onClick={handleClickCopy} className="my-4">
+              Copy
+            </Button>
+          </div>
+          <div className="text-center">Tweet Text</div>
+          <div className="text-center">Text Length: {textLength} / 280</div>
+          <div
+            dangerouslySetInnerHTML={{
+              __html: tweetText.replace(/\n/g, "<br>"),
+            }}
+          />
+        </div>
+      )}
     </div>
   );
 }
